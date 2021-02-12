b37593c2c2bdb262457756fceff47a699443d0c4
LoSt-SoFtware <75952429+LoSt-SoFtware@users.noreply.github.com>
2021-02-12 04:01:03 +0300

    Add files via upload

---
+++ Birkari.py
@@ -0,0 +1,37 @@
+import time
+
+print("\x1b[31;1mBosTa Ta Tool AkreTo")
+time.sleep(1)
+print(" \033[92mTawaw Tool Krayawa")
+
+
+#!/usr/bin/python2
+#coding=utf-8
+#The Credit For This Code Goes To MaTin
+#If You Wanna Take Credits For This Code, Please Look Yourself Again...
+#Reserved2021
+logo = """
+\x1b[31;1m  __  __       _______ _____ _   _ 
+\x1b[31;1m |  \/  |   /\|__   __|_   _| \ | |
+\x1b[31;1m | \  / |  /  \  | |    | | |  \| |
+\x1b[31;1m | |\/| | / /\ \ | |    | | | . ` |
+\x1b[31;1m | |  | |/ ____ \| |   _| |_| |\  |
+\x1b[31;1m |_|  |_/_/    \_\_|  |_____|_| \_|
+\033[92m                                
+
+Note : Tool  Zaxm Bo Birkari
+Note : Create By MaTin
+Note : Telegram : @MaTin_Cod3r
+Github : https://github.com/Matin-Cod3r
+
+
+
+"""
+print(logo)
+
+z = int(input("FIRST NUMBER: "))
+b = int(input("SECOND NUMBER: "))
+
+matin = z+b
+
+print(matin)
